ed1c8472833acc0ef1dba46a683bb220ee1b3c0d
Amanda Walker <walker.akw@gmail.com>
2020-07-08 23:58:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,11 @@ def line(place)
   place_array = []
   if array.length == 0
    puts "The line is currently empty."
- else 
+ else
    place.each do |tix|
      place_array << "Hello, you are currently number #{tix +1} in line."
    end #each
  end #if
 end #line
-   
+
   
