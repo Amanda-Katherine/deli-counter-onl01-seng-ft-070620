277eb1e37adaf46916bbc250ca940d0c3d938c4e
Amanda Walker <walker.akw@gmail.com>
2020-07-09 00:26:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,14 @@
 katz_deli = []
 
 def line(katz_deli)
-  place = "The line is currently"
+
   if katz_deli.length == 0
-   puts place << " empty."
+   puts "The line is currently empty."
  else
    katz_deli.each_with_index do |name, number|
+     place = "The line is currently: "
      if number == katz_deli.length - 1
-       place << ": #{number +1}. #{name} "
+       place << "#{number +1}. #{name} "
      else
        place << "#{number +1}. #{name}"
      end #if
