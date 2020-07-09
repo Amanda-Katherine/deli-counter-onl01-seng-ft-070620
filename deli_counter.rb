1026a77eba5b538802530b7bd5056c2d0795d9c8
Amanda Walker <walker.akw@gmail.com>
2020-07-09 00:35:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,22 @@
 # Write your code here.
 katz_deli = []
-
-def line(katz_deli)
-
-  if katz_deli.length == 0
-   puts "The line is currently empty."
- else
-   place = "The line is currently: "
-   katz_deli.each_with_index do |name, number|
-     if number == katz_deli.length - 1
-       place << "#{number +1}. #{name}"
-     else
-       place << "#{number +1}. #{name}"
-     end #if
-   end #each
-   puts place
- end #if
-
-end #line
+num = 2
+num += 3
+puts num
+# def line(katz_deli)
+# 
+#   if katz_deli.length == 0
+#    puts "The line is currently empty."
+#  else
+#    place = "The line is currently: "
+#    katz_deli.each_with_index do |name, number|
+#      if number == katz_deli.length - 1
+#        place << "#{number +1}. #{name}"
+#      else
+#        place << "#{number +1}. #{name} "
+#      end #if
+#    end #each
+#    puts place
+#  end #if
+# 
+# end #line
